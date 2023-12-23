89e9375559ea003b785260fc6d0578e1f4bb9026
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-22 16:14:50 -1000

    Update arrays.js

--- JavaScript Crash Course/arrays.js
+++ JavaScript Crash Course/arrays.js
@@ -29,6 +29,8 @@ if (del == "yes") {
 alert(arr);
 */
 
+/*
+// Lesson 3
 const arr = [];
 const arr2 = [1, 2, 3];
 const arr3 = [3, 4, 5, ["Hello"]]; // 2 dimensional Array
@@ -36,4 +38,25 @@ const arr3 = [3, 4, 5, ["Hello"]]; // 2 dimensional Array
 arr.push(arr2);
 arr.push(arr3);
 
+
 console.log(arr[1][3][0]); // logs "Hello"
+*/
+
+// Lesson 4
+const arr = [45, 23, 33, -8, 7];
+//const stringArr = arr.toString();
+// arr.unshift(-1); places at the beginning
+// arr.shift(5); places at the end
+
+// arr.sort() sorts by 1 char at a time
+// arr.sort((a, b) => a - b); // Sort by ascending (custom function sort)
+// arr.sort((a, b) => b - a); // Sort by descending (custom function sort)
+
+// const arr2 = ["Hello", "world"];
+//const arr3 = arr.concat(arr2); // Join together the arrays
+
+// const removed = arr.splice(2, 3); removes 3 elements starting at index 2
+// const str = arr.join(" | "); // Customizes whats placed in between the elements
+// const sl = arr.slice(1, 4); // Goes up to index 4 not including 4
+
+console.log(sl);
