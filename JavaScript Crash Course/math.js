58d33695f5e66f538dc7de4ea6ad1ffbf37b8dcf
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-27 21:35:48 -1000

    Create math.js

---
+++ JavaScript Crash Course/math.js
@@ -0,0 +1,32 @@
+/*
+// Lesson 1
+const num = 2.45;
+// const roundedNum = Math.round(num); // Rounds to nearest int
+// const roundedNum = Math.round(num * 10) / 10; // Rounds to nearest .10 decimal place
+//const roundedNum = Math.floor(num); // Always rounds down
+const roundedNum = Math.ceil(num); // Always rounds up
+console.log(roundedNum);
+*/
+
+/*
+// Lesson 2
+const x = 10;
+const y = 20;
+const arr = [1, 2, 3, 4, 5, 6];
+
+const max = Math.max(x, y, 25);
+const min = Math.min(x, y, 42);
+const minArr = Math.min(...arr);
+const maxArr = Math.max(...arr);
+console.log("max: " + max);
+console.log("min: " + min);
+console.log(minArr);
+console.log(maxArr);
+*/
+
+//const abs = Math.abs(-9);
+//const abs = Math.log(-9); // log10, log2
+//console.log(abs);
+
+const x = Infinity; // Treated as largest possible value ompared with other numbers
+const y = Infinity; // Treated as smallest possible value compared with other numbers
