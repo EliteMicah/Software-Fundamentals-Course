98c945b786c8931716d9677ef99dd6f6efd94381
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-27 17:49:58 -1000

    Update eventsThis.js

--- JavaScript Crash Course/eventsThis.js
+++ JavaScript Crash Course/eventsThis.js
@@ -36,3 +36,40 @@ function test() {
 }
 
 test();
+
+/*
+// Lesson 3
+//const func = (x, y) => x + y;
+const func = (x, y) => ({ x: x, y: y });
+
+const result = func(1, 2);
+console.log(result);
+*/
+
+// Lesson 4
+function getName() {
+  return this.firstName + " " + this.lastName;
+}
+
+// "this" accesses the global object
+const getName = () => {
+  return this.firstName + " " + this.lastName;
+};
+
+const person = {
+  firstName: "Tim",
+  lastName: "Ruscica",
+  getName: getName,
+};
+
+const person2 = {
+  firstName: "Joe",
+  lastName: "Smith",
+  getName: getName,
+};
+
+const personName = person.getName();
+console.log(personName);
+
+const personName2 = person2.getName();
+console.log(personName2);
