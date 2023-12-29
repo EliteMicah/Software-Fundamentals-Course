2868adee0263a51150b22c4439603be62375718a
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-28 20:16:11 -1000

    Update question4.js

--- JavaScript Crash Course/Practice Questions/question4.js
+++ JavaScript Crash Course/Practice Questions/question4.js
@@ -14,12 +14,12 @@ Spacing = “1”
 
 let userString = "coursecareers";
 const delimiter = "|";
-// const spacing = 3;
+const spacing = 1;
 
 let newString = "";
 
 for (let i = 0; i < userString.length; i++) {
-  if (i % 1 == 0 && i != 0) {
+  if (i % spacing == 0 && i != 0) {
     newString += delimiter;
   }
   newString += userString[i];
