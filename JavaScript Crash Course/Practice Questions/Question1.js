54a8b0091f293c4997fcff9ee12c453e84bd3f4e
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-28 15:29:54 -1000

    Create Question1.js

---
+++ JavaScript Crash Course/Practice Questions/Question1.js
@@ -0,0 +1,8 @@
+// Odd or Even Number
+const userInput = prompt("Enter a random whole number");
+
+if (Number(userInput) % 2 == 0) {
+  alert("Number is Even");
+} else {
+  alert("Number is Odd");
+}
