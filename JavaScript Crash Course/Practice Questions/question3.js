6353a6601271d134dcc2ccdd4cc3e9392d2ba3dd
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-28 19:55:49 -1000

    Update question3.js

--- JavaScript Crash Course/Practice Questions/question3.js
+++ JavaScript Crash Course/Practice Questions/question3.js
@@ -1,3 +1,17 @@
+/*
+Example 1: 
+Min = 2, max = 7 
+~Program outputs: “[2, 3, 4, 5, 6, 7]”~ 
+
+Example 2: 
+Min = -3, Max = -9 
+~Program outputs: “[ ]”~ this is because the max is less than the min 
+
+Example 3: 
+Min = -1, Max = -1 
+~Program outputs: “[-1]”~
+*/
+
 let userMin = -1;
 const userMax = -1;
 
