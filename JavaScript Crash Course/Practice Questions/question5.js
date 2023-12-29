03b3951bf50c2b453492992d383c1860c9b7df00
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-28 20:36:39 -1000

    Update question5.js

--- JavaScript Crash Course/Practice Questions/question5.js
+++ JavaScript Crash Course/Practice Questions/question5.js
@@ -1,3 +1,13 @@
+/*
+Example 1: 
+mathIsFun(“12|13|-4|5|”) -> 25 
+// the numbers are 12, 13, -4 and 5. The largest sum of two numbers is 12 + 13 = 25 
+
+Example 2: 
+mathIsFun(“-2|-4|-1|-1”) -> -2 
+// the numbers are -2, -4, -1 and -1. The largest sum of two numbers is -1 + -1 = -2 
+*/
+
 const userString = "-2|-4|-1|-1";
 const arr = userString.split("|");
 
