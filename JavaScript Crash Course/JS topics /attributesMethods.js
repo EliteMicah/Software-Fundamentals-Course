32809b758773ee838252390377949142befe5101
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-01-09 11:43:36 -0800

    Update attributesMethods.js

--- JavaScript Crash Course/JS topics /attributesMethods.js
+++ JavaScript Crash Course/JS topics /attributesMethods.js
@@ -33,3 +33,38 @@ class Teacher {
 
 const teacher = new Teacher("Joe", 24, 65000);
 //teacher.setSalary(-10);
+
+// "static" lesson
+class Person {
+  name;
+  // Static is associated with the class not the instance
+  static numberOfPeople = 0;
+
+  constructor(name) {
+    this.name = name;
+    // "this" does not work with static in js
+    Person.numberOfPeople++;
+  }
+
+  static getNumberOfPeople() {
+    // "this" does work when inside a static function
+    if (this.numberOfPeople >= 10) this.removePerson();
+    return this.numberOfPeople;
+  }
+
+  static removePerson() {
+    this.numberOfPeople--;
+  }
+
+  // static doesn't need it to be related to the instance
+  // static can be related to class
+  // This class relies on that static function to be used inside of here
+  static calculator(n) {
+    return n ** 2;
+  }
+}
+
+const p = new Person("Tim");
+const p2 = new Person("Joe");
+Person.removePerson();
+console.log(Person.getNumberOfPeople());
