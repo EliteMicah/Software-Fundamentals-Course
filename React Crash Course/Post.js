d381080928fe2658d83fd403b0e79830fe6cd422
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-01-25 11:09:56 -0800

    Update Post.js

--- React Crash Course/Post.js
+++ React Crash Course/Post.js
@@ -7,3 +7,29 @@ export function Post({ title, author, content }) {
     </div>
   );
 }
+/*
+import "./post.css";
+
+export function Post({ title, author, content, following }) {
+  const button = !following ? (
+    <button>Follow</button>
+  ) : (
+    <button>Unfollow</button>
+  );
+  return (
+    <div className="card">
+      <h1>{title}</h1>
+      <h1>By: {author}</h1>
+      {button}
+      <p>{content}</p>
+    </div>
+  );
+}
+/*
+      {<button> {following ? "Unfollow" : "Follow"} </button>}
+      {Used for 2 different types of button
+      following ? <button>Unfollow</button> : <button>Follow</button>}
+      {following && <button>Unfollow</button>}
+*/
+
+*/
