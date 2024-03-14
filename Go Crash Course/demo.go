3ca29078225577b55371bcd500c0f789c11410c1
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-03-14 15:25:33 -0700

    Create demo.go

---
+++ Go Crash Course/demo.go
@@ -0,0 +1,24 @@
+package main
+
+// Output to console
+import "fmt"
+
+func main() {
+
+	var x uint8 = 8
+	fmt.Println(x)
+
+}
+
+// Mac Go Command
+// go run demo.go
+// ./demo
+
+// int Positive numbers
+// uint Negative numbers & positive
+// float64 Floating point value - decimal
+// rune = int32
+// bool true or false
+// string ""
+// char ''
+// nill undefined or null
