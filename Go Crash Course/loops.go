c7e04781d51b6bbf5918907eaea8884c0f5fb020
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-04-01 17:53:37 -0700

    Create loops.go

---
+++ Go Crash Course/loops.go
@@ -0,0 +1,35 @@
+package main
+
+// Output to console
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
+
+// for idx := 0; idx < 10; idx++ {
+// 	fmt.Println(idx)
+// }
+
+// a := 1
+// for a < 10 {
+// 	fmt.Println("Loop")
+// 	a++
+// }
+
+// ASCII = 1 byte 256
+// UTF-8 = 4 byte (emojis etc.)
+
+// _ placeholder value for no error
+
+// str := "hello world"
+
+// for idx := 0; idx < len(str); idx++ {
+// 	fmt.Printf("%c", str[idx])
+// }
+
+// for _, char := range str {
+// 	fmt.Printf("%c", char)
