83dd7cb9a7d339376674593d019b14ad4ab721a8
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-04-01 17:54:22 -0700

    Create ifStatement.go

---
+++ Go Crash Course/ifStatement.go
@@ -0,0 +1,33 @@
+package main
+
+// Output to console
+import "fmt"
+
+func main() {
+
+	var x uint8 = 8
+	fmt.Println(x)
+
+}
+
+// IF STATEMENT
+// 	x := 2
+//	if x < 3 {
+//		fmt.Println(x)
+//	} else if x > 4 {
+//		fmt.Println("Greater than")
+//	} else {
+//		fmt.Println("Hi")
+//	}
+
+// SWITCH STATEMENT
+// a := 1
+// switch {
+// case a < 1:
+// 	fmt.Println("one")
+// 	fallthrough // Automatically goes to next case
+// case a > 2:
+// 	fmt.Println("two")
+// default:
+// 	fmt.Println("defualt")
+// }
