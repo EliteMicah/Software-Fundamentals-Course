36173f433f73bd9d303e355cc494522edf2c1299
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-05-14 18:13:49 -1000

    Update code.test.js

--- JavaScript Crash Course/testing/code.test.js
+++ JavaScript Crash Course/testing/code.test.js
@@ -11,3 +11,26 @@ describe("Test add function", () => {
         expect(result).toEqual(3);
     })
 })
+
+// -----------------------------
+
+const code = require("./code");
+
+describe("removeEven", () => {
+
+    // "all" runs before all the testing cases
+    // "each" runs before each test case
+    beforeAll(() => {
+        console.log("Before");
+    });
+
+    test("Filters out even numbers from array", () => {
+        const numbers = [1,2,3,4,5,6];
+        const result = code.removeEven(numbers);
+        expect(result).toEqual([1,3,5]);
+    });
+
+    afterAll(() => {
+        console.log("After");
+    });
+});
