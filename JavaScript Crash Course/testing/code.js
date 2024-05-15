274af4a8ce73575dc9f0590e3a2b57c549854382
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2024-05-14 18:14:14 -1000

    Update code.js

--- JavaScript Crash Course/testing/code.js
+++ JavaScript Crash Course/testing/code.js
@@ -5,3 +5,13 @@ function add(x, y) {
 module.exports = {
     add, 
 }
+
+// --------------------------
+
+function removeEven(numbers) {
+    return numbers.filter(number => number % 2 !== 0);
+}
+
+module.exports = {
+    removeEven, 
+}
